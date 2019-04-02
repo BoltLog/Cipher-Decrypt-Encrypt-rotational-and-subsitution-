2c1bd58dcd5c15cf38a648a0eabc4e0454786d66
BoltLog <49173285+BoltLog@users.noreply.github.com>
2019-04-02 13:46:45 +1100

    start of a basic menu heap of work required still

--- Cipher.c
+++ Cipher.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
+//this code aims to achieve encryption and decrytion of any text into uppercase letters
+//this program however will only accept text not any special characters or numbers
+//this code will allow encryyption and decryption in rotstional and subsitution ciphers
+//it will achieve this with file and standard input and output with a user interface
+// follow instructions when prompted to and the program should work
 
+int main() {
+ 
+    printf("Cipher Selection\n");
+    printf(" a) rotation cipher\n");
+    printf(" b) subsitution cipher\n");
+
+    char c;
+    scanf("%c", &c);
+    while(c< 'a' || c> 'b') {
+        scanf("%c", &c);
+        switch(c){
+
+        case 'a': printf(" a) rotation cipher\n");
+        break;
+        case 'b': printf(" b) subsitution cipher\n");
+        break;
+        default: printf("an invalid output '%c' detected\n please enter a or b\n", c);
+    }
+
+    }
+    return 0;
 }
 
 
